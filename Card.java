381ea5e3cac2bf0c6ec540048678045403802ba3
Campbell <campbellzc@s.dcsdk12.org>
2013-10-29 09:07:58 -0600

    finish and print a full deck for homework

---
+++ Card.java
@@ -0,0 +1,10 @@
+public class Card {
+	private String card;
+	private int number;
+	int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+ 	public Card(String card, int value) {
+		this.card = card;
+		this.number = number;
+
+	}
+}
---
+++ Deck.java
@@ -0,0 +1,23 @@
+public class Deck {
+	private 
+
+	public Deck() {
+		
+
+	}
+	public void suit(String suit) {
+		Card ace = new Card("Ace", 1, 11);
+		Card two = new Card("Two",2);
+		Card three = new Card("Three",3);
+		Card four = new Card("Four",4);
+		Card five = new Card("Five", 5);
+		Card six = new Card("Six", 6);
+		Card seven = new Card("Seven",7);
+		Card eight = new Card("Eight",8);
+		Card nine = new Card("Nine",9);
+		Card ten = new Card("Ten", 10);
+		Card jack = new Card("Jack", 10);
+		Card queen = new Card("Queen", 10);
+		Card king = new Card("King", 10);
+	}
+}
