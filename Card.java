c3462d08c4e7b5ba34dca0a856864b8cac850c07
Zack <campbellzc@s.dcsdk12.org>
2013-10-30 21:01:52 -0600

    prints out a deck of cards

--- Card.java
+++ Card.java
@@ -1,10 +1,33 @@
 public class Card {
 	private String card;
-	private int number;
+	private int value;
+	private String suit;
+	//private int number;
 	int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
- 	public Card(String card, int value) {
-		this.card = card;
-		this.number = number;
+ 	public Card(String card, int value, String suit) {
+		setCard(card);
+		setSuit(suit);
+		setValue(value);
+		System.out.println("" + this.card + " of " + this.suit);
 
 	}
+	public String getCard() {
+		return this.card;
+	}
+	public void setCard(String card) {
+		this.card = card;
+	}
+	public int getValue() {
+		return this.value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getSuit() {
+		return this.suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
 }
--- Deck.java
+++ Deck.java
@@ -1,23 +1,27 @@
 public class Deck {
-	private 
+	private String suit;
 
+	
 	public Deck() {
-		
-
+		suit("Diamonds");
+		suit("Clubs");
+		suit("Spades");
+		suit("Hearts");
 	}
 	public void suit(String suit) {
-		Card ace = new Card("Ace", 1, 11);
-		Card two = new Card("Two",2);
-		Card three = new Card("Three",3);
-		Card four = new Card("Four",4);
-		Card five = new Card("Five", 5);
-		Card six = new Card("Six", 6);
-		Card seven = new Card("Seven",7);
-		Card eight = new Card("Eight",8);
-		Card nine = new Card("Nine",9);
-		Card ten = new Card("Ten", 10);
-		Card jack = new Card("Jack", 10);
-		Card queen = new Card("Queen", 10);
-		Card king = new Card("King", 10);
+		Card ace = new Card("Ace", 1, suit);
+		Card two = new Card("Two",2, suit);
+		Card three = new Card("Three",3, suit);
+		Card four = new Card("Four",4, suit);
+		Card five = new Card("Five", 5, suit);
+		Card six = new Card("Six", 6, suit);
+		Card seven = new Card("Seven",7, suit);
+		Card eight = new Card("Eight",8, suit);
+		Card nine = new Card("Nine",9, suit);
+		Card ten = new Card("Ten", 10, suit);
+		Card jack = new Card("Jack", 10, suit);
+		Card queen = new Card("Queen", 10, suit);
+		Card king = new Card("King", 10, suit);
+
 	}
 }
